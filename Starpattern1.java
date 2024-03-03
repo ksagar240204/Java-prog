3044a97ce014521ea50be777a449ead30864a9eb
K.SAGAR <161337484+ksagar240204@users.noreply.github.com>
2024-03-03 18:46:25 +0530

    Add files via upload

---
+++ Starpattern1.java
@@ -0,0 +1,17 @@
+public class Starpattern1
+{
+	public static void main(String[] args)
+	{
+		int i,j;
+		
+		for(i=1;i<=5;i++)
+		{
+			for(j=1;j<=5;j++)
+			{
+				System.out.print("*");
+			}
+		System.out.println();
+		}
+	
+	}
+}
---
+++ Starpattern3.java
@@ -0,0 +1,16 @@
+public class Starpattern3
+{
+	public static void main(String[] args)
+	{
+		int i,j;
+		
+		for(i=1;i<=5;i++)
+		{
+			for(j=i;j<=5;j++)
+			{
+				System.out.print("*");
+			}
+		System.out.println();
+		}
+	}
+}
